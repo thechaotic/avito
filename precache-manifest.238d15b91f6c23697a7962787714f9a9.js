0ca9159330484a7aea54e054e38ebadc656e28c0
thechaotic <mixnba@mail.ru>
2020-02-03 22:17:42 +0300

    Updates

---
+++ precache-manifest.238d15b91f6c23697a7962787714f9a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c28103627df6ce12feece100e4c45f",
+    "url": "/avito/index.html"
+  },
+  {
+    "revision": "14c1e80636ade2f388e2",
+    "url": "/avito/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d600abcbe3ff3d48b38b",
+    "url": "/avito/static/css/main.3fc5f104.chunk.css"
+  },
+  {
+    "revision": "14c1e80636ade2f388e2",
+    "url": "/avito/static/js/2.6951fc2f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/avito/static/js/2.6951fc2f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d600abcbe3ff3d48b38b",
+    "url": "/avito/static/js/main.c3cfdbb4.chunk.js"
+  },
+  {
+    "revision": "02d2ac558e1752940c0d",
+    "url": "/avito/static/js/runtime-main.19f6a186.js"
+  }
+]);
