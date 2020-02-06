77f157f9d0ea05daa53d74a57c7fc55249be2e95
thechaotic <mixnba@mail.ru>
2020-02-06 21:36:02 +0300

    new

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Modal from 'react-bootstrap/Modal';
-import axios from 'axios';
+
 
 
 
@@ -14,12 +14,13 @@ class App extends React.Component{
       pictureId: 0,
       bigImages: [],
       show: false,
-      big: []
+      info: [],
+      
     }
     this.handleShow = this.handleShow.bind(this);
     this.handleClose = this.handleClose.bind(this);
     this.handleGetIdImage = this.handleGetIdImage.bind(this);
-    // this.handleGetBigImage = this.handleGetBigImage.bind(this);
+    
   }
   handleClose() {
 		this.setState({ show: false });
@@ -29,23 +30,19 @@ class App extends React.Component{
 		this.setState({ show: true });
   }
   handleGetIdImage(e){
-    this.setState({pictureId: e.target.id})
-    fetch('https://boiling-refuge-66454.herokuapp.com/images/'+this.state.pictureId)
+    
+    
+    fetch('https://boiling-refuge-66454.herokuapp.com/images/'+e.target.id)
     .then(result => result.json())
     .then(data => {
-        this.setState({ bigImages: data})
-        console.log(this.state.bigImages)
+        this.setState({ bigImages: data,
+                        info: data.comments[0],
+                        })
+        // console.log(this.state.bigImages)
     })
     
   }
-  // handleGetBigImage(){
-  //   fetch('https://boiling-refuge-66454.herokuapp.com/images/'+this.state.pictureId)
-  //   .then(result => result.json())
-  //   .then(data => {
-  //       this.setState({ bigImages: data})
-  //       console.log(this.state.bigImages)
-  //   })
-  // }      
+ 
     
     
   
@@ -68,7 +65,7 @@ class App extends React.Component{
 
   render(){   
     
-    console.log(this.state.bigImages)
+    console.log(this.state.info)
     return( 
       <div className="container">
           <header>
@@ -88,22 +85,36 @@ class App extends React.Component{
                         src={element.url}
                         id={element.id}
                         onClick={this.handleGetIdImage}
-                        // onClick={this.handleGetBigImage}
                        />
                     </div>
                   ))}    
                 </div>   
                 <Modal show={this.state.show} onHide={this.handleClose}>
 					        <Modal.Header closeButton>
-						        <Modal.Title>Modal heading</Modal.Title>
+						        
 				      	</Modal.Header>
-					        <Modal.Body>
-                    Woohoo, you're reading this text in a modal!
+					        <Modal.Body >
+                  <div className="container">
+                    <div className="row">  
+                      <div className="col-md-8 col-lg-8  bigimage">
+                        <img
+                          className="img-fluid"
+                          src={this.state.bigImages.url}
+                        >
+                        </img> 
+                      </div>
+                        <div className="col-md-4 col-lg-4 coment">
+                  <p></p>
+                          <p>{this.state.info.text}</p>
+                        </div>
+                          <div className="col-md-8 col-lg-8  coment">
+                            <input type="text" className="col-md-12 col-lg-12 mt-4 "/>
+                            <input type="text" className="col-md-12 col-lg-12 mt-4 "/>
+                            <button className="col-md-12 col-lg-12 mt-4 ">Оставить комментарий</button>
+                          </div>
+                    </div>
+                    </div>
                   </Modal.Body>
-                  <Modal.Footer>
-              
-              
-                  </Modal.Footer>
 				        </Modal>
               </main>
             </div>
